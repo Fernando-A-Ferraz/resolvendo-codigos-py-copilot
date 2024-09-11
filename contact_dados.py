37af65958359048157a84f0df39d8c469d03a036
Fernando Augusto Ferraz <100580357+Fernando-A-Ferraz@users.noreply.github.com>
2024-09-11 13:59:42 +0000

    Dados concatenados

---
+++ contact_dados.py
@@ -0,0 +1,9 @@
+# Vamos receber dois dados diferentes do usuário e concatenar-]
+
+info1 = input("Digite o primeiro dado: ")
+info2 = input("Digite o segundo dado: ")
+
+info_concatenada = info1 + " " + info2
+
+print("Dados concatenados: ", info_concatenada)
+
